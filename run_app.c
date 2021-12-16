0e787a5aef700da87521df29772abbf556dad479
ChrisMuteb <ibond.chris@gmail.com>
2021-12-16 23:32:24 +0200

    Phase1---Init=>cmd argument option

---
+++ run_app.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>//used for exit
+#include<getopt.h>
+
+void error_message(){
+    printf("Required: -f fileInName.txt | tem -o fileOutName.txt\n");
+    exit(2);// exit with an error code of 2
+}
+
+int main(int argc, char *argv[]){
+    if(argc == 4){
+        error_message();
+    }
+    //Variable declaration
+    int optionInput;
+    //Implementation
+    
+    while((optionInput = getopt(argc, argv, "f:o:")) != -1){
+        printf("Arguments %s", optarg);
+        //printf("argv: %s", *argv);
+        optionInput = -1;
+    }
+    
+}
