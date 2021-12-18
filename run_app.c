9f99012ac54983a6f7a47cd0c93dbce76fb9b4ed
ChrisMuteb <ibond.chris@gmail.com>
2021-12-18 22:36:13 +0200

    successful opening of input file
    From getopt, the input and output files have been passed successfully on Linux and the school server. All data from the input.txt has been saved into the str char string for futher manipulation purporses. It will then be placed into the LinkedList.

--- run_app.c
+++ run_app.c
@@ -1,10 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>//used for exit
 #include<getopt.h>
+#include<string.h>
+
+#define str_in_dt_sz 100 //string input data size
+
+char str[str_in_dt_sz];//holds the input data from the input txt file
 
 void error_message(){
     printf("Required: -f <fileInName.txt> | -o f<ileOutName.txt>\n");
-    exit(2);// exit with an error code of 2
+    exit(2);
+}
+//open the input file and returns a string of all data in the file
+void in_fl_openFile(char *in_txt, char str[]){
+
+    FILE *f_i;
+
+    f_i = fopen(in_txt,"r");
+
+    if(f_i != NULL){
+        char c;
+        char s[100];
+
+        int i = 0;
+        while((c=fgetc(f_i)) != EOF){
+           if(c == '\n'){
+               c = '#';
+               s[i] = c;
+           }
+           if(c != '\n'){
+               s[i] = c;
+           }
+           i++;
+            //putchar(c);
+        }
+        fclose(f_i);
+        strcpy(str, s);
+        //printf("CHris: %s",str);
+    }else printf("Error opening file.\n");
+
 }
 
 int main(int argc, char *argv[]){
@@ -13,7 +47,10 @@ int main(int argc, char *argv[]){
         error_message();
     }
     //Variable declaration
-    int optionInput;
+    int optionInput;//Getopt var. holder
+    char *in_fl, *out_fl;
+
+
     //Implementation
     
     while((optionInput = getopt(argc, argv, "f:o:")) != -1){
@@ -21,17 +58,23 @@ int main(int argc, char *argv[]){
         switch (optionInput)
         {
         case 'f':
-            printf("f: %s\n", optarg);
+            in_fl = optarg;
             break;
         case 'o':
-            printf("o: %s\n", optarg);
+            out_fl = optarg;
             break;
         default:
             error_message();
             break;
         }
         
-        //optionInput = -1;
     }
+    //printf("f: %s\n", in_fl);
+    in_fl_openFile(in_fl, str);// str hold data frm input.txt==> 5:2:3#4:1:2# ...
+    printf("CHris: %s",str);
+    //printf("o: %s\n", out_fl);
+
+
+    exit(0);
     
 }
