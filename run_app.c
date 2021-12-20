0aae8c64da8674e738867e0853ae2736ba2c153c
ChrisMuteb <ibond.chris@gmail.com>
2021-12-21 01:15:21 +0200

    read from file and filled the linked list
    Read from the input file(input.txt) with a different approach than the previous commit then filled the linked list. I displayed also the data just to make sure that it works well.

--- run_app.c
+++ run_app.c
@@ -3,9 +3,7 @@
 #include<getopt.h>
 #include<string.h>
 
-#define str_in_dt_sz 100 //string input data size
-
-char str[str_in_dt_sz];//holds the input data from the input txt file
+#define str_in_dt_sz 1024 //string input data size
 
 void error_message(){
     printf("Required: -f <fileInName.txt> | -o f<ileOutName.txt>\n");
@@ -40,15 +38,32 @@ void in_fl_openFile(char *in_txt, char str[]){
     }else printf("Error opening file.\n");
 
 }
+////incoming-------20 Dec
+typedef struct process{
+    int title;//Stores process ID
+    int burt_time;
+    int arrival_time;
+    int priority;
+    struct process *next;
+}p;
+static int num = 0;//Assign an id to a newly created process
+
+//Function Prototype
+struct process *createNode(int b_t, int a_t, int priorty); 
+struct process *insertBack(struct process *header, int b_t, int a_t, int priorty);
+void display(struct process *header);
+void build_llist(char *in_txt, struct process *hdr);//gets data from file and fills up the linked list
 
 int main(int argc, char *argv[]){
-    if(argc < 4){
+
+    if(argc < 4){//ensures that the number of arguments is not less than 4
         //show error message
         error_message();
     }
-    //Variable declaration
+    
     int optionInput;//Getopt var. holder
     char *in_fl, *out_fl;
+    struct process *header = NULL;
 
 
     //Implementation
@@ -69,12 +84,77 @@ int main(int argc, char *argv[]){
         }
         
     }
-    //printf("f: %s\n", in_fl);
-    in_fl_openFile(in_fl, str);// str hold data frm input.txt==> 5:2:3#4:1:2# ...
-    printf("CHris: %s",str);
-    //printf("o: %s\n", out_fl);
+    
+    build_llist(in_fl, header);
 
 
     exit(0);
     
+}
+
+void build_llist(char *in_txt, struct process *hdr){
+    FILE *spin;
+    int b_t, a_t, priority;
+    char in[str_in_dt_sz];
+   
+
+
+    spin = fopen(in_txt, "r");//open file in reading mode
+
+    if(!spin){
+        printf("Could not open file\n");
+        exit(1);
+    }
+ 
+    while(fgets(in, 1024, spin)){
+
+        b_t = atoi(strtok(in, ":"));//stores digits bfr the ':'
+        a_t = atoi(strtok(NULL, ":"));//stores digits bfr the ':' starting at the point it left off
+        priority = atoi(strtok(NULL, "\n"));
+        hdr = insertBack(hdr, b_t, a_t, priority);
+    }
+
+    display(hdr);
+
+}
+struct process *createNode(int b_t, int a_t, int priorty){
+    struct process *temp;
+    temp = (struct  process*)malloc(sizeof(struct process));
+    temp->title = ++num;
+    temp->burt_time = b_t;
+    temp->arrival_time = a_t;
+    temp->priority = priorty;
+    temp->next = NULL;
+
+    return temp;
+}
+struct process *insertBack(struct process *header, int b_t, int a_t, int priorty){
+
+    //create node
+    struct process *temp = createNode(b_t, a_t, priorty);
+    struct process *headertemp;//start a 1st node
+    if(header == NULL){
+        header = temp;
+        return header;
+    }
+    //Find the end of list
+    headertemp = header;
+    while (headertemp->next != NULL){
+        headertemp = headertemp->next;
+    }
+    
+    headertemp->next = temp;
+    return header;
+}
+void display(struct process *header){
+    
+  if(header == NULL)
+        puts("List is emplty");
+    struct process *temp = header;
+    while (temp != NULL)
+    {
+        printf("Process%d: %d-%d-%d -->", temp->title, temp->burt_time, temp->arrival_time, temp->priority);
+        temp = temp->next;
+    }
+    puts("");
 }
